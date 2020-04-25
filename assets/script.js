e170cf27a74232a5b1256731db2f4a2cc4c973ff
andytheelf <ballew22@gmail.com>
2020-04-25 10:37:08 -0500

    html updates, jquery, bootstrap, local storage add

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,45 @@
-const apiKey = '533ace453fe24f86c272d95fa98d9637';
-function myFunction() {
-    const citySearch = document.querySelector("#citySearch").value;
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + citySearch + '&APPID=' + apiKey)
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
+$(document).ready(() => {
+    //local storage
+    let pastCities =JSON.parse(localStorage.getItem("savedCities")) || [];
+    for (let i = pastCities.length -1; i > 0; i--) {
+        addButton(pastCities[i]);
+    }
+
+    if (pastCities[0] !=null || pastCities[0] != undefined) {
+          getWeather(pastCities[0]);
+    }
+
+    // api call
+    $("#cityButton").click(function(event) {
+        event.preventDefault();
+
+        if ($("#cityInput").val() != "") {
+            getWeather(titleCaseConvert($("#cityInput").val()));
+            $("#cityInput").val("");
+        }
     });
-  }
+
+    //weather input
+    function getWeather(checkCity) {
+        let forecastUrl = "https://api.openweathermap.org/data/2.5/weather";
+        let fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast";
+        let apiKey = "appid=533ace453fe24f86c272d95fa98d9637";
+        let unitMeasure = "&units=Imperial";
+        let cities = "?q=" + checkCity;
+
+        $.ajax({
+            url: forcastUrl + city + unitMeasure + "&" + apiKey + "&mode=json",
+            method: "GET",
+            success: function() {
+                addButton(checkCity);
+            },
+            error: function() {
+                alert("Information Unavailable");
+            }
+        }).then(function(response){
+            weatherUpdate(response);
+        });
+
+
+    }
+});
